e17086edbf876bc6d718c14ff175b4d357b2a0c6
LUCAS\monto <lucasmontoby@gmail.com>
2023-03-08 09:49:53 -0300

    Clase 1-Fecha 07-03-23

---
+++ clase1.js
@@ -0,0 +1,44 @@
+// Ejercicio 1
+// function duplicar (numero){
+//     return numero*2
+// }
+
+// console.log(duplicar(5))
+
+// Ejercicio 2
+// let numeros = [1,2,3,4]
+// let resultado = 0
+
+// for (let i = 0; i < numeros.length; i++ ){
+//     resultado = resultado + numeros[i]
+// }
+
+// console.log(resultado)
+
+// resultado += numeros[i]
+
+// Ejercicio 3
+// let texto = "hola mundo"
+
+// function mayuscula(str){
+//     str = str.toUpperCase();
+//     return str;
+// }
+
+// console.log(mayuscula(texto))
+// --------------------------------------
+// let texto;
+
+// function mayuscula(texto){
+//     return    texto = String(texto).toUpperCase()
+// }
+
+// console.log(mayuscula('hola'))
+
+// Ejercicio 4
+// function contar(objeto){
+//     return Object.keys(objeto).length;
+// }
+
+// console.log(contar({nombre: "Juan", edad: 30, ciudad: "Buenos Aires"}))
+
